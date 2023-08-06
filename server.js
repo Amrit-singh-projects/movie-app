dfc12054bf1ac63645b6e00eabf44305a9a628f4
Amrit Singh <amritsingh220894@gmail.com>
2023-08-07 00:04:28 +0530

    first commit

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 5000 
+const PORT = process.env.PORT || 5000 
 
 
 app.use(express.static('public'));
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
